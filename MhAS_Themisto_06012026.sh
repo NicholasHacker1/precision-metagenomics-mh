69cf41da542b1626d1916fa2eed4980932bad021
NicholasHacker1 <nchlshacker@gmail.com>
2026-02-17 21:21:54 -0600

    Update MhAS_Themisto_06012026.sh

--- MhAS_Themisto_06012026.sh
+++ MhAS_Themisto_06012026.sh
@@ -1,28 +1,15 @@
 #! /bin/bash
-#-----------------------------------------
-#SBATCH --job-name=MhAS_ThemistoPseudoalignment
-
-#SBATCH --nodes=1
-#SBATCH --tasks-per-node=48
-#SBATCH --mem=360GB
-#SBATCH --time=24:00:00  
-#SBATCH --output=MhAS_Themisto_%j.out
-#SBATCH --mail-user=nhacker@tamu.edu
-#SBATCH --mail-type=ALL
-
-#-----------------------------------------
 # Load necessary modules and set environment variables
-
 
 module load GCCcore/12.2.0
 
-mkdir -p /scratch/user/nhacker/Mh_Adaptive_Sampling/Themisto/Themisto_output
+mkdir -p /path/to/output
 
-for fq in /scratch/user/nhacker/Mh_Adaptive_Sampling/Centrifuge/Classification_Results/centrifuge_fastq/*.fastq; do
+for fq in /path/to/input; do
     echo "Processing file: ${fq##*/}"
     fq_basename=${fq##*/} # Remove path
     fq_brief=${fq_basename%%.fastq*} # Remove .fastq extension
     # Run Themisto pseudoalignment on the fastq file
-    /scratch/user/nhacker/Mh_Isolate_Paper/Themisto/themisto/build/bin/themisto pseudoalign -q $fq -i /scratch/user/nhacker/Mh_Isolate_Paper/Themisto/Themisto_Mh_Indices/2025_themisto_index_no --temp-dir /scratch/user/nhacker/Mh_Isolate_Paper/Themisto/temp -t 48 --threshold 0.95> /scratch/user/nhacker/Mh_Adaptive_Sampling/Themisto/Themisto_output/${fq_brief}_themisto.txt
+    /path/to/themisto pseudoalign -q $fq -i /scratch/user/nhacker/Mh_Isolate_Paper/Themisto/Themisto_Mh_Indices/2025_themisto_index_no --temp-dir /scratch/user/nhacker/Mh_Isolate_Paper/Themisto/temp -t 48 --threshold 0.95> /scratch/user/nhacker/Mh_Adaptive_Sampling/Themisto/Themisto_output/${fq_brief}_themisto.txt
     echo "Finished processing file: ${fq_basename}, themisto pseudoalignment output saved to /scratch/user/nhacker/Mh_Adaptive_Sampling/Themisto/Themisto_output/${fq_brief}_themisto.txt"
 done
